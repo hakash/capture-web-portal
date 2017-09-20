565896e833e9f3e05d1607269efc71358c86d8b9
Morten Olsrud <post@dittdesign.com>
2017-09-20 13:52:19 +0200

    .

--- scripts/gjestenett.js
+++ scripts/gjestenett.js
@@ -68,13 +68,15 @@ var CWP = {
 		xhr.open("POST", backendURL, true);
 		
 		xhr.onreadystatechange = function() {
-			if(xhr.readyState === 4 && xhr.status === 200) {
-				document.getElementById(type + "-status").innerText = "Melding sendt!";
-				console.log(xhr.response);
-			}
-			else {
-				console.log(xhr.status);
-				console.log(xhr.response);
+			if(xhr.readyState === 4){
+				if( xhr.status === 200) {
+					document.getElementById(type + "-status").innerText = "Melding sendt!";
+				}
+				else {
+					console.log(xhr.status);
+					console.log(xhr.response);
+					console.log(JSON.parse(xhr.response));
+				}
 			}
 		}
 		xhr.send(payload);
