fdb8ed659809757d0d062b273e729e335e79abd7
Morten Olsrud <post@dittdesign.com>
2018-10-18 21:05:14 +0200

    Removed Facebook functionality. Added favicon.

--- scripts/gjestenett.js
+++ scripts/gjestenett.js
@@ -3,14 +3,10 @@ var CWP = {
 	formtypes : [
 		"mobile",
 		"email",
-		"guest",
-		"facebook"
+		"guest"
 	],
 
 	params : {
-	},
-
-	FaceBook : {
 	},
 
 	autherr : 0,
@@ -71,9 +67,7 @@ var CWP = {
 				if(element != null){
 					element.value = this.params[key];
 				}
-				if(this.formtypes[i] != "facebook"){
-					document.getElementById(this.formtypes[i] + "-id").value = this.getCookie(this.formtypes[i] + "-id");				
-				}
+				document.getElementById(this.formtypes[i] + "-id").value = this.getCookie(this.formtypes[i] + "-id");
 			}
 		}
 	},
@@ -84,7 +78,7 @@ var CWP = {
 		if(this.params.autherr && this.params.autherr === "1"){
 			if(location.href.indexOf("#") > -1){
 				var hash = location.href.split("#")[1];
-				if(hash === "facebookForm" || hash === "autherrModal"){
+				if(hash === "autherrModal"){
 					hash = "";
 				}
 				document.getElementById("autherrButtonClose").href = "#" + hash;
@@ -104,105 +98,13 @@ var CWP = {
 		}
 	},
 
-	fbSetupComplete : function(){
-		this.setFacebookButtonAsLogin();
-		
-		this.checkFaceBookLogin();
-	},
-
-	setFacebookButtonAsLogin : function(){
-		document.getElementById("fb-button").onclick = function(event){
-			FB.login(CWP.fblogin, { scope:"public_profile,email"} );
-		};
-		document.getElementById("fb-button-text").innerHTML = "Logg inn";
-	},
-
-	setFacebookButtonAsLogout: function(){
-		document.getElementById("fb-button").onclick = function(event){
-			FB.logout(CWP.fblogout);
-		};
-		document.getElementById("fb-button-text").innerHTML = "Logg ut";
-	},
-
-	checkFaceBookLogin : function(autologin){
-		FB.getLoginStatus(function(response) {
-			CWP.FBStatusCallback(response, autologin);
-		});
-	},
-
-	FBStatusCallback : function(response, autologin){
-		
-		if(response.status === 'connected' ){
-			// user logged in and approved our app.
-			// welcome the user and redirect to page in uri
-			CWP.FaceBook.token = response.authResponse.accessToken;
-			CWP.FaceBook.password = SHA256(response.authResponse.accessToken);
-			CWP.setFacebookButtonAsLogout();
-			
-			FB.api("/me",function(response){
-				CWP.FaceBook.email = response.email;
-				CWP.FaceBook.name = response.name;
-
-				document.getElementById("facebook-username").value = CWP.FaceBook.email;
-				document.getElementById("facebook-password").value = CWP.FaceBook.password;
-				document.getElementById("facebook-name").innerHTML = CWP.FaceBook.name;
-
-				if(autologin){
-					CWP.registerFacebookUser(autologin);
-				}
-				else {
-					CWP.showFacebookForm();
-				}
-			},{'fields':'name,email'});
-		}
-		else if(response.status === 'not_authorized'){
-			// user logged in to FB, but has not approved this app
-			// log them in.
-			CWP.setFacebookButtonAsLogin();
-		}
-		else if(response.status === 'unknown'){
-			// user not logged in to FB
-			// log them in.
-			CWP.setFacebookButtonAsLogin();
-		}
-	},
-
 	login : function(type){
 		console.log("ref: " + document.referrer);
 		CWP.setCookie("logging_in",1, 1);			
 
-		if(type === "facebook"){
-			CWP.registerFacebookUser(true);
-		}
-		else {
-			CWP.setCookie("last_hash", type + "Form", 30 * 24 * 60);
-			CWP.setCookie(type + "-id", document.getElementById(type + "-id").value, 30 * 24 * 60);
-			document.querySelector("#" + type + "Form form").submit();
-		}
-	},
-
-	fblogin : function(){
-		CWP.checkFaceBookLogin(true);
-	},
-
-	fblogout : function(){
-		CWP.checkFaceBookLogin();
-	},
-
-	loginFacebook : function(){
-		document.querySelector("#facebookForm form").submit();
-	},
-
-	showFacebookForm : function(){
-		if(!this.autherr || this.autherr !== 1){
-			console.log("...");
-			document.getElementById("autherrButtonClose").href = "#";			
-			window.location.href = window.location.href.split("#")[0] + "#facebookForm";
-		}
-		else {
-			//document.getElementById("autherrButtonClose").href = "#facebookForm";
-		}
-		//console.log(this.autherr + " : (" + typeof(this.autherr) + ")" );
+		CWP.setCookie("last_hash", type + "Form", 30 * 24 * 60);
+		CWP.setCookie(type + "-id", document.getElementById(type + "-id").value, 30 * 24 * 60);
+		document.querySelector("#" + type + "Form form").submit();
 	},
 
 	getCode : function(type){
@@ -244,34 +146,6 @@ var CWP = {
 			console.log(response);
 		}
 		return false;
-	},
-
-	registerFacebookUser : function(autologin){
-		var url = "/facebookuser";
-
-		var data = {};
-		data.uri = JSON.stringify(this.params);
-		data.userid = this.FaceBook.email;
-		data.password = this.FaceBook.password;
-
-		this.jsonPost(url, data, function(response){
-			// success
-			if(autologin){
-				CWP.loginFacebook();
-			}
-			else {
-				CWP.showFacebookForm();
-			}
-		},
-		function(response){
-			// error
-			var msg = "Det oppstod en feil ved registreringen av Facebook-kontoen din i \
-						våre systemer. Vennligst prøv en annen innloggingsmetode.";
-
-			document.querySelector("#facebookForm > p").innerText = msg;
-			document.querySelector("#facebookForm form input[type='submit']").style = "display:none;";
-			CWP.showFacebookForm();
-		});
 	},
 
 	jsonPost : function(url, data, success, error){
